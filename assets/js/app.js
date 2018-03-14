27452d8ebd96ee5a8ccc5f664cb12d78485f67fc
Niennis <estefania.osses.v@gmail>
2018-03-13 21:27:30 -0300

    Guía de estilos añadida e indentacion corregida

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,24 +6,24 @@ function pokeSubmit() {
   
   fetch('https://pokeapi.co/api/v2/pokemon/?limit=949')
     .then(function(response) {
-      //Turns the the JSON into a JS object
+      // Turns the the JSON into a JS object
       return response.json();
     })
     .then(function(data) {
-      let pokeInput = ($("#pokeInput").val()).toLowerCase();
+      let pokeInput = ($('#pokeInput').val()).toLowerCase();
       let pokeCount = 0;
       console.log(pokeInput);
-      for (var i = 0; i < data.results.length; i++){
-        if (pokeInput === data.results[i].name){
+      for (var i = 0; i < data.results.length; i++) {
+        if (pokeInput === data.results[i].name) {
           pokeCount++;
           let pokeInfo = data.results[i].url ;
           fetch(pokeInfo).then(function(result) {
             return result.json();
-          })
-          .then(function(pokemon) {
+          });
+          then(function(pokemon) {
             let pokeData = '';
             let types = [];
-            for(let i = 0; i < pokemon.types.length; i++){
+            for (let i = 0; i < pokemon.types.length; i++) {
               types.push(pokemon.types[i].type.name.capitalize());
             }              
             pokeData = `<div class="row">
@@ -57,40 +57,40 @@ function pokeSubmit() {
                   </tbody>
                 </table>
               </div>
-            </div>`
-              showInfo.append(pokeData);              
-        })        
-      } 
-    }    
-    if (pokeCount === 0) {
-      showInfo.append(`<div class="row">
-        <div class="col-lg-6 col-md-6 offset-lg-5 offset-md-5 col-sm-12 col-xs-12 pokeContainer">
-          <p>Pokemon not found. Try again!</p>
-        </div>
-      </div>`
-      )
-    }  
-  });
+            </div>`;
+            showInfo.append(pokeData);              
+          });        
+        } 
+      }    
+      if (pokeCount === 0) {
+        showInfo.append(`<div class="row">
+                          <div class="col-lg-6 col-md-6 offset-lg-5 offset-md-5 col-sm-12 col-xs-12 pokeContainer">
+                            <p>Pokemon not found. Try again!</p>
+                          </div>
+                        </div>`
+        );
+      };  
+    });
   fetch('https://pokeapi.co/api/v2/pokemon-species/?limit=802').then(function(moreResponse) {
     return moreResponse.json();
   })
-  .then(function(moreData) {
-    let pokeInput = ($("#pokeInput").val()).toLowerCase();
-    for (var i = 0; i < moreData.results.length; i++){
-      if (pokeInput === moreData.results[i].name){    
-        let pokeMoreInfo = moreData.results[i].url ;
-        fetch(pokeMoreInfo).then(function(result) {
-          return result.json();
-        })
-        .then(function(pokemonPlus) {       
-          let plus = '';
-          let flavor = '';
-          for(let i = 0; i < 5; i++){
-            if ( pokemonPlus.flavor_text_entries[i].language.name === 'en') {
-              flavor = pokemonPlus.flavor_text_entries[i];          
-            }
-          }
-          plus = `<div class="row">
+    .then(function(moreData) {
+      let pokeInput = ($('#pokeInput').val()).toLowerCase();
+      for (var i = 0; i < moreData.results.length; i++) {
+        if (pokeInput === moreData.results[i].name) {    
+          let pokeMoreInfo = moreData.results[i].url ;
+          fetch(pokeMoreInfo).then(function(result) {
+            return result.json();
+          })
+            .then(function(pokemonPlus) {       
+              let plus = '';
+              let flavor = '';
+              for (let i = 0; i < 5; i++) {
+                if (pokemonPlus.flavor_text_entries[i].language.name === 'en') {
+                  flavor = pokemonPlus.flavor_text_entries[i];          
+                }
+              }
+              plus = `<div class="row">
                     <div class="col-lg-6 col-md-6 offset-lg-5 offset-md-5 col-sm-12 col-xs-12 pokeContainer">
                       <table class="table">
                         <tbody>
@@ -109,16 +109,15 @@ function pokeSubmit() {
                         </tbody>
                       </table>
                       </div></div>`
-          $('#moreInfo').append(plus);
-        })
-        .catch(function(error) {
-          console.log('Something went wrong: ' + error);
-        }) 
-      };  
-    }
-  });
+              $('#moreInfo').append(plus);
+            })
+            .catch(function(error) {
+              console.log('Something went wrong: ' + error);
+            });
+        };  
+      }
+    });
 }
-
 
 // Functión para poner en mayúscula la primera letra
 String.prototype.capitalize = function() {
